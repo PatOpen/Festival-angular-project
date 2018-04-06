47afa6110ee800dce9e69106bb5daaf948e42632
Pat <patopenclassrom@gmail.com>
2018-04-06 13:01:29 +0200

    Projet festival angular
    - Structure de l'application sans design

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { InscriptionComponent } from './insciption/inscription.component';
+import { FilmsListComponent } from './films-list/films-list.component';
+import { FilmDetailComponent } from './films-list/film-detail/film-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { ActusComponent } from './actus/actus.component';
+import { ActusFormComponent } from './actus/actus-form/actus-form.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import {AuthService} from './services/auth.service';
+import {AuthGuardService} from './services/auth-guard.service';
+import {ActusService} from './services/actus.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { ActusAccueilComponent } from './actus/actus-accueil/actus-accueil.component';
+
+const appRoutes: Routes = [
+    { path: 'auth/Enregistrement', canActivate: [AuthGuardService], component: SignupComponent },
+    { path: 'auth/Connexion', component: SigninComponent },
+    { path: 'auth/Admin-inscription', canActivate: [AuthGuardService], component: InscriptionComponent },
+    { path: 'auth/Admin-actus', canActivate: [AuthGuardService], component: ActusComponent },
+    { path: 'auth/Admin-nouvelle-actus', canActivate: [AuthGuardService], component: ActusFormComponent },
+    { path: 'Films', component: FilmsListComponent },
+    { path: 'Films/Detail/:id', component: FilmDetailComponent },
+    { path: 'Accueil', component: AccueilComponent },
+    { path: '', redirectTo: 'Accueil', pathMatch: 'full' },
+    { path: '**', redirectTo: 'Accueil' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilmsListComponent,
+    FilmDetailComponent,
+    HeaderComponent,
+    ActusComponent,
+    ActusFormComponent,
+    SignupComponent,
+    SigninComponent,
+    AccueilComponent,
+    InscriptionComponent,
+    ActusAccueilComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+      AuthService,
+      AuthGuardService,
+      ActusService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
